c26402620bdbe2cd4b1c633cafa6fa399b5106ac
hewhounknown <zwemarnhtay@gmail.com>
2024-06-18 00:45:13 +0630

    Add project files.

---
+++ Tic_Tac_Toe/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+char[] arr = new char[10];
+
+for(int i = 0;  i < arr.Length; i++)
+{
+    arr[i] = ' ';
+}
+
+string dashboard = "     |     |      " + "\n" +
+                  $"  {arr[1]}  |  {arr[2]}  |   {arr[3]}  "+ "\n" +
+                   "_____|_____|_____ " + "\n" +
+                   "     |     |      " + "\n" +
+                  $"  {arr[4]}  |  {arr[5]}  |   {arr[6]}  " + "\n" +
+                   "_____|_____|_____ " + "\n" +
+                   "     |     |      " + "\n" +
+                 $"  {arr[7]}  |  {arr[8]}  |   {arr[9]}  " + "\n" +
+                   "     |     |      " + "\n";
+
+Console.WriteLine(dashboard);
+Console.ReadKey();
